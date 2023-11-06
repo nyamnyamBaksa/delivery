f2ff5fb7b20eab27abe59b87387474dadb2578e3
imgonnabe <qorlwn456123@gmail.com>
2023-11-06 10:19:02 +0900

    Fix: 오류 해결
    오류 해결

--- src/main/java/com/delivery/web/store/StoreController.java
+++ src/main/java/com/delivery/web/store/StoreController.java
@@ -2,10 +2,14 @@ package com.delivery.web.store;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
@@ -13,17 +17,51 @@ public class StoreController {
 
 	@Autowired
 	private StoreService storeService;
-	
-	@GetMapping("/storelist")
-	public String storelist(Model model) {
-		
-		List<StoreDTO> list = storeService.list();
-		System.out.println(list);
-		
+
+	@GetMapping("/food/{mncate}")
+	public String storelist(@PathVariable int mncate, Model model) {
+
+		List<StoreDTO> list = storeService.list(mncate);
+
 		model.addAttribute("list", list);
 		return "storelist";
 	}
+
+	@GetMapping("/food/storedetail")
+	public String storedetail(@RequestParam(value = "sno", required = false) int sno, Model model) {
+
+		StoreDTO detail = storeService.detail(sno);
+		List<MenuDTO> menulist = storeService.getmenulist(sno);
+
+		model.addAttribute("detail", detail);
+		model.addAttribute("menulist", menulist);
+
+		System.out.println(menulist);
+		return "storedetail";
+	}
+
+	@GetMapping("/food/storeinfo")
+	public String storeinfo(@RequestParam(value = "sno", required = false) int sno, Model model) {
+
+		List<StoreDTO> storeinfo = storeService.storeinfo(sno);
+		
+		model.addAttribute("storeinfo", storeinfo);
+
+		return "storeinfo";
+	}
+	
+	
+	@GetMapping("/food/menudetail")
+	public String menudatail(@RequestParam(value = "mnno", required = false) int mnno, Model model) {
+		
+		 List<MenuDTO> menudetail = storeService.menudetail(mnno);
+
+		model.addAttribute("menudetail", menudetail);
+
+		//System.out.println(menudetail);
+		
+		return "menudetail";
+	}
 	
 
-	
 }
--- src/main/java/com/delivery/web/store/StoreService.java
+++ src/main/java/com/delivery/web/store/StoreService.java
@@ -10,13 +10,44 @@ import org.springframework.stereotype.Service;
 public class StoreService {
 	
 	@Autowired
-	private StoreDAO categorylistDAO;
+	private StoreDAO storeDAO;
+	
 
-	public List<StoreDTO> list() {
-		
-		return categorylistDAO.list();
+	public List<StoreDTO> list(int mncate) {
+		return storeDAO.list(mncate);
 	}
 
+
+	public StoreDTO detail(int sno) {
+		
+		StoreDTO storeDTO = storeDAO.detail(sno);
+		List<MenuDTO> menulist = storeDAO.getMenuList(sno);
+		
+		storeDTO.setMenuList(menulist);
+		
+		return storeDTO;
+	}
+
+
+	public List<MenuDTO> getmenulist(int sno) {
+		
+		return storeDAO.getMenuList(sno);
+	}
+
+
+	public List<StoreDTO> storeinfo(int sno) {
+		
+		return storeDAO.storeinfo(sno);
+	}
+
+
+	public List<MenuDTO> menudetail(int mnno) {
+		
+		return storeDAO.menudetail(mnno);
+	}
+
+
 	
+
 
 }
